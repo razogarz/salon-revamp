2244ba94bfc5900986b79de92add171436738a01
Razogarz <patryknapek@gmail.com>
2023-02-09 16:38:52 +0100

    navbar dziala

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,40 +1,44 @@
-import Burgir from '../images/burgirS.png';
+import BurgirSmall from '../images/burgirS.png';
+import BurgirBig from '../images/burgir.png';
 import { useRef, createRef } from 'react';
 
 let hamburgerIcon = createRef<HTMLButtonElement>();
 let hamburgerMenu = createRef<HTMLDivElement>();
-function fun(){
-    console.log('siema');
-}
-function toggleHamburger(ev:any){
+function toggleHamburger(){
     console.log('click');
     if(hamburgerMenu.current?.classList.contains('hidden')){
         hamburgerMenu.current?.classList.remove('hidden');
+        hamburgerMenu.current?.classList.add('flex');
     } else {
         hamburgerMenu.current?.classList.add('hidden');
+        hamburgerMenu.current?.classList.remove('flex');
     }
 }
 export default function Navbar() {
     return (
-        <div className='text-center'>
-            <div className='lg:text-4xl md:text-2xl md:block hidden m-auto bg-primary text-primary'>
+        <div>
+            <div className='lg:text-4xl md:text-2xl md:block hidden m-auto bg-primary text-primary text-center'>
                 <button className='px-6 py-4'>O nas</button>
                 <button className='px-6 py-4'>Godziny Otwarcia</button>
                 <button className='px-6 py-4'>Kontakt</button>
                 <button className='px-6 py-4'>Galeria</button>
             </div>
-            <div className=' md:invisible flex justify-between p-4 bg-primary text-primary text-2xl'>
-                <p>
-                    Twój Styl
-                </p>
-                <button ref={hamburgerIcon} >
-                    <img src={Burgir} onClick={fun} />
-                </button>
-                <div ref={hamburgerMenu} className='hidden'>
-                    <button>O nas</button>
-                    <button>Godziny Otwarcia</button>
-                    <button>Kontakt</button>
-                    <button>Galeria</button>
+            <div className=' md:invisible p-4 bg-primary text-primary text-2xl'>
+                <div className='flex justify-between py-3'>
+                    <p className='sm:text-4xl'>
+                        Twój Styl
+                    </p>
+                    <button ref={hamburgerIcon} 
+                        onClick={toggleHamburger} >
+                        <img className='sm:hidden block' src={BurgirSmall} />
+                        <img className='sm:block hidden' src={BurgirBig} />
+                    </button>
+                </div>
+                <div ref={hamburgerMenu} className='hidden flex-col'>
+                    <button className='text-left py-3'>O nas</button>
+                    <button className='text-left py-3'>Godziny Otwarcia</button>
+                    <button className='text-left py-3'>Kontakt</button>
+                    <button className='text-left py-3'>Galeria</button>
                 </div>
             </div>
         </div>
