34ab14e30e57916db9d50bec708b23a7f19ecc5a
Razogarz <patryknapek@gmail.com>
2023-02-11 15:53:36 +0100

    content finished

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,9 +18,9 @@ export default function Navbar() {
     return (
         <div className='mb-0'>
             <div className='lg:text-4xl md:text-2xl md:block hidden m-auto bg-primary text-primary text-center'>
-                <button className='px-6 py-4 italic'>O nas</button>
-                <button className='px-6 py-4 italic'>Godziny Otwarcia</button>
-                <button className='px-6 py-4 italic'>Kontakt</button>
+                <button className='px-6 py-4 italic'><a href="#onas">O nas</a></button>
+                <button className='px-6 py-4 italic'><a href="#godziny">Godziny Otwarcia</a></button>
+                <button className='px-6 py-4 italic'><a href="#kontakt">Kontakt</a></button>
                 <button className='px-6 py-4 italic'><a href='/galery'>Galeria</a></button>
             </div>
             <div className=' md:hidden p-4 bg-primary text-primary text-2xl'>
@@ -35,9 +35,9 @@ export default function Navbar() {
                     </button>
                 </div>
                 <div ref={hamburgerMenu} className='sm:text-3xl hidden flex-col absolute z-10 bg-primary left-0 w-full px-4 py-4'>
-                    <button className='text-left py-3'>O nas</button>
-                    <button className='text-left py-3'>Godziny Otwarcia</button>
-                    <button className='text-left py-3'>Kontakt</button>
+                    <button className='text-left py-3'><a href="#onas">O nas</a></button>
+                    <button className='text-left py-3'><a href="#godziny">Godziny Otwarcia</a></button>
+                    <button className='text-left py-3'><a href="#kontakt">Kontakt</a></button>
                     <button className='text-left py-3'>Galeria</button>
                 </div>
             </div>
