7a70db2910dcd3c651041874e6d3d6193331cdb9
Razogarz <patryknapek@gmail.com>
2023-02-11 16:03:03 +0100

    dodana petla po galerii

---
+++ src/components/photoGallery.tsx
@@ -0,0 +1,21 @@
+/* 
+    RENDER ALL IMAGES IN THE GALLERY
+*/
+function renderGallery(){
+    let gallery = [];
+    for(let i = 1; i <= 42; i++){
+        gallery.push(
+            <a href={`src/images/maxi-img/pic${i}.jpg`}>
+                <img src={`src/images/mini-img/pic${i}.jpg`}/>
+            </a>
+        );
+    }
+    return gallery;
+}
+export default function PhotoGallery() {
+    return (
+        <div id="photoGallery" className='flex flex-wrap'>
+            {renderGallery()}
+        </div>
+    );
+}
