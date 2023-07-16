bf9318a217a85f3ca4595b4a274b15c3a81446a8
Patryk Knapek <patrykknapek@MacBook-Pro-Patryk.local>
2023-07-16 15:15:44 +0200

    gallery fixed

---
+++ src/components/images.tsx
@@ -0,0 +1,89 @@
+import pic1 from '../images/mini-img/pic1.jpg'
+import pic2 from '../images/mini-img/pic2.jpg'
+import pic3 from '../images/mini-img/pic3.jpg'
+import pic4 from '../images/mini-img/pic4.jpg'
+import pic5 from '../images/mini-img/pic5.jpg'
+import pic6 from '../images/mini-img/pic6.jpg'
+import pic7 from '../images/mini-img/pic7.jpg'
+import pic8 from '../images/mini-img/pic8.jpg'
+import pic9 from '../images/mini-img/pic9.jpg'
+import pic10 from '../images/mini-img/pic10.jpg'
+import pic11 from '../images/mini-img/pic11.jpg'
+import pic12 from '../images/mini-img/pic12.jpg'
+import pic13 from '../images/mini-img/pic13.jpg'
+import pic14 from '../images/mini-img/pic14.jpg'
+import pic15 from '../images/mini-img/pic15.jpg'
+import pic16 from '../images/mini-img/pic16.jpg'
+import pic17 from '../images/mini-img/pic17.jpg'
+import pic18 from '../images/mini-img/pic18.jpg'
+import pic19 from '../images/mini-img/pic19.jpg'
+import pic20 from '../images/mini-img/pic20.jpg'
+import pic21 from '../images/mini-img/pic21.jpg'
+import pic22 from '../images/mini-img/pic22.jpg'
+import pic23 from '../images/mini-img/pic23.jpg'
+import pic24 from '../images/mini-img/pic24.jpg'
+import pic25 from '../images/mini-img/pic25.jpg'
+import pic26 from '../images/mini-img/pic26.jpg'
+import pic27 from '../images/mini-img/pic27.jpg'
+import pic28 from '../images/mini-img/pic28.jpg'
+import pic29 from '../images/mini-img/pic29.jpg'
+import pic30 from '../images/mini-img/pic30.jpg'
+import pic31 from '../images/mini-img/pic31.jpg'
+import pic32 from '../images/mini-img/pic32.jpg'
+import pic33 from '../images/mini-img/pic33.jpg'
+import pic34 from '../images/mini-img/pic34.jpg'
+import pic35 from '../images/mini-img/pic35.jpg'
+import pic36 from '../images/mini-img/pic36.jpg'
+import pic37 from '../images/mini-img/pic37.jpg'
+import pic38 from '../images/mini-img/pic38.jpg'
+import pic39 from '../images/mini-img/pic39.jpg'
+import pic40 from '../images/mini-img/pic40.jpg'
+import pic41 from '../images/mini-img/pic41.jpg'
+import pic42 from '../images/mini-img/pic42.jpg'
+
+const images = [
+	pic1,
+	pic2,
+	pic3,
+	pic4,
+	pic5,
+	pic6,
+	pic7,
+	pic8,
+	pic9,
+	pic10,
+	pic11,
+	pic12,
+	pic13,
+	pic14,
+	pic15,
+	pic16,
+	pic17,
+	pic18,
+	pic19,
+	pic20,
+	pic21,
+	pic22,
+	pic23,
+	pic24,
+	pic25,
+	pic26,
+	pic27,
+	pic28,
+	pic29,
+	pic30,
+	pic31,
+	pic32,
+	pic33,
+	pic34,
+	pic35,
+	pic36,
+	pic37,
+	pic38,
+	pic39,
+	pic40,
+	pic41,
+	pic42
+]
+
+export default images;
--- src/components/photoGallery.tsx
+++ src/components/photoGallery.tsx
@@ -1,20 +1,23 @@
 /* 
     RENDER ALL IMAGES IN THE GALLERY
 */
-import placeholderImage from '../images/mini-img/pic1.jpg';
+import images from './images'
 
 
 function renderGallery() {
-    let gallery = [];
-    for (let i = 1; i <= 42; i++) {
+    let gallery: any = [];
+    let index = 1;
+    images.forEach((image) => {
         gallery.push(
-            <div className="w-2/5 m-4 shadow-2xl md:w-1/6" key={i}>
-                <a href={placeholderImage}>
-                    <img src={placeholderImage} />
+            <div className="w-2/5 m-4 shadow-2xl md:w-1/6" key={index}>
+                <a href={image}>
+                    <img src={image} />
                 </a>
             </div>
         );
-    }
+        index++;
+    });
+
     return gallery;
 }
 export default function PhotoGallery() {
