8749fe5f00fac5082d8316a11f947888df4f4bd4
Patryk Knapek <patrykknapek@MacBook-Pro-Patryk.local>
2023-07-16 14:00:02 +0200

    final for netlify

--- src/components/photoGallery.tsx
+++ src/components/photoGallery.tsx
@@ -1,14 +1,16 @@
 /* 
     RENDER ALL IMAGES IN THE GALLERY
 */
+import placeholderImage from '../images/mini-img/pic1.jpg';
+
 
 function renderGallery() {
     let gallery = [];
     for (let i = 1; i <= 42; i++) {
         gallery.push(
             <div className="w-2/5 m-4 shadow-2xl md:w-1/6" key={i}>
-                <a href={`src/images/mini-img/pic${i}.jpg`}>
-                    <img src={`src/images/mini-img/pic${i}.jpg`} />
+                <a href={placeholderImage}>
+                    <img src={placeholderImage} />
                 </a>
             </div>
         );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'background-image': 'url("src/images/mini-img/zdjSalonu2-min.jpg")'
+			}
+		},
 	},
 	plugins: [],
 }
